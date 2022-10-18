d75e6dc7cdaff999d2840fa7196a5fcf443627f0
Philani Mxathule <3911755@myuwc.ac.za>
2022-10-18 09:48:36 +0200

    initial commit

---
+++ elseif.py
@@ -0,0 +1,37 @@
+# x = 3
+# r = x % 2
+
+# if r==0:
+#     print("Even") 
+#     if x>5:
+#         print("Great")
+#     else:
+#         print("Not so great")
+        
+        
+# else:
+#     print("Odd")
+
+# print("Bye")
+
+# if False:
+#     print("Im right")
+    
+    
+# print("Bye")
+
+x = 7
+
+if(x==1):
+    print("One")
+
+elif(x==2):
+    print("Two")
+    
+elif(x==3):
+    print("Three")
+
+elif(x==4):
+    print("Four")
+else:
+    print("Wrong input")
